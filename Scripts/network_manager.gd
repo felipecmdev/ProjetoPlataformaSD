71a4cb90ea01d570b0f959c611013b20d9b9f6ab
PedroDias-2214 <pedrodgs014@gmail.com>
2026-05-02 11:18:40 -0300

    Cliente agora não laga mais pois faz a própria predição, Menu é a cena principal e a mecanica do empurrão foi criada. Players não interagem mais com a hitbox de outros

--- Scripts/network_manager.gd
+++ Scripts/network_manager.gd
@@ -108,12 +108,23 @@ func poll_receive_host() -> void:
 			udp.set_dest_address(ip_cliente, porta_cliente)
 			udp.put_packet(msg_welcome.to_utf8_buffer())
 		
-		elif mensagem_texto.begins_with("I|"):
+		elif mensagem_texto.begins_with("P|"):
 			var parts := mensagem_texto.split("|")
-			if parts.size() >= 3:
-				clients_input[client_id]["move_x"] = clampf(float(parts[1]), -1.0, 1.0)
-				clients_input[client_id]["jump"] = (parts[2] == "1")
-
+			if parts.size() >= 5:
+				if players_nodes.has(client_id):
+					var nodo_cliente = players_nodes[client_id]
+					if is_instance_valid(nodo_cliente):
+						nodo_cliente.global_position = Vector2(float(parts[1]), float(parts[2]))
+						nodo_cliente.velocity = Vector2(float(parts[3]), float(parts[4]))
+		
+		elif mensagem_texto.begins_with("K|"):
+			_process_knockback(mensagem_texto)
+			var buffer = mensagem_texto.to_utf8_buffer()
+			for key in connected_clients.keys():
+				var dados_cliente = connected_clients[key]
+				udp.set_dest_address(dados_cliente["ip"], dados_cliente["port"])
+				udp.put_packet(buffer)
+				
 func poll_receive_client() -> void:
 	while udp.get_available_packet_count() > 0:
 		var mensagem_recebida := udp.get_packet().get_string_from_utf8()
@@ -127,6 +138,9 @@ func poll_receive_client() -> void:
 				
 		elif mensagem_recebida.begins_with("S|"):
 			_apply_snapshot(mensagem_recebida)
+		
+		elif mensagem_recebida.begins_with("K|"):
+			_process_knockback(mensagem_recebida)
 
 func send_snapshot_host() -> void:
 	if connected_clients.is_empty() or players_nodes.is_empty():
@@ -161,13 +175,16 @@ func send_input_client() -> void:
 	if not players_nodes.has(my_player_id):
 		return
 		
-	var direcao_x := Input.get_axis(&"move_left", &"move_right")
-	var estado_pulo := 1 if Input.is_action_just_pressed(&"jump") else 0
-	
+	var meu_personagem = players_nodes[my_player_id]
+	var mensagem_pos := "P|%f|%f|%f|%f" % [
+		meu_personagem.global_position.x,
+		meu_personagem.global_position.y,
+		meu_personagem.velocity.x,
+		meu_personagem.velocity.y
+	]
 	# Pacote do cliente
-	var mensagem_input := "I|%f|%d" % [direcao_x, estado_pulo]
 	udp.set_dest_address(join_address, port)
-	udp.put_packet(mensagem_input.to_utf8_buffer())
+	udp.put_packet(mensagem_pos.to_utf8_buffer())
 
 func _apply_snapshot(mensagem_estado: String) -> void:
 	var partes_pacote := mensagem_estado.split("|")
@@ -180,6 +197,11 @@ func _apply_snapshot(mensagem_estado: String) -> void:
 			var nodo_jogador = players_nodes[id_jogador]
 			
 			if is_instance_valid(nodo_jogador):
+				
+				if (id_jogador == my_player_id):
+					i += 5
+					continue
+				
 				nodo_jogador.global_position = Vector2(
 					float(partes_pacote[i+1]),
 					float(partes_pacote[i+2])
@@ -203,6 +225,34 @@ func _on_physics_frame_end() -> void:
 	elif role == Role.CLIENT:
 		poll_receive_client()
 		send_input_client()
+
+func send_push_command(target_id: int, push_dir: float) -> void:
+	var msg := "K|%d|%f" % [
+		target_id,
+		push_dir
+	]
+	
+	if role == Role.HOST:
+		_process_knockback(msg)
+		var buffer = msg.to_utf8_buffer()
+		for key in connected_clients.keys():
+			var dados = connected_clients[key]
+			udp.set_dest_address(dados["ip"], dados["port"])
+			udp.put_packet(buffer)
+	
+	else:
+		udp.set_dest_address(join_address, port)
+		udp.put_packet(msg.to_utf8_buffer())
+
+func _process_knockback(msg: String) -> void:
+	var parts = msg.split("|")
+	if parts.size() >= 3:
+		var target_id = int(parts[1])
+		var push_dir = float(parts[2])
+		
+		if target_id == my_player_id and players_nodes.has(my_player_id):
+			players_nodes[my_player_id].receive_knockback(push_dir)
+	
 
 func _exit_tree() -> void:
 	udp.close()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED := 130.0
+const JUMP_VELOCITY := -300.0
+const COOLDOWN_EMPURRAO := 1.0
 
 var player_id: int = -1
 
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var facing_direction: float = 1.0
+var push_cooldown: float = 0.0
 
 func _ready() -> void:
 	add_to_group("players")
@@ -20,21 +23,18 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	_disable_passive_collision()
+
 	if player_id == NetworkManager.my_player_id:
+		if push_cooldown > 0:
+			push_cooldown -= delta
 		_handle_local_input()
-		
-	elif NetworkManager.is_host() and NetworkManager.clients_input.has(player_id):
-		_handle_network_input()
-	
-	else:
-		pass
-		
-	if NetworkManager.is_host() or player_id == NetworkManager.my_player_id:
-		_push_other_player()
+		_check_active_push()
 		move_and_slide()
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = !(velocity.x < 0)
+		facing_direction = signf(velocity.x)
 
 func _handle_local_input() -> void:
 	if Input.is_action_just_pressed(&"jump") and is_on_floor():
@@ -45,32 +45,29 @@ func _handle_local_input() -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-func _handle_network_input() -> void:
-	var input_remoto = NetworkManager.clients_input[player_id]
-	
-	if input_remoto["jump"] and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-		input_remoto["jump"] = false #reseta o pulo pra não sair voando
-	
-	var direction = input_remoto["move_x"]
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-func _push_other_player() -> void:
+func _disable_passive_collision() -> void:
 	var others := get_tree().get_nodes_in_group("players")
 	for other in others:
-		if other == self:
-			continue
-		var diff: Vector2 = other.global_position - global_position
-		if diff.length() < 16.1:
-			var push_dir := signf(diff.x)
-			if push_dir == 0:
-				push_dir = 1.0
-			other.velocity.x += push_dir * 140.0  #força da empurrada
-
+		if other != self:
+			add_collision_exception_with(other)
+	
+func _check_active_push() -> void:
+	if Input.is_action_just_pressed(&"push") and push_cooldown <= 0.0:
+		push_cooldown = COOLDOWN_EMPURRAO
+		var others := get_tree().get_nodes_in_group("players")
+		for other in others:
+			if other == self:
+				continue
+		
+			if global_position.distance_to(other.global_position) < 40:
+				NetworkManager.send_push_command(other.player_id, facing_direction)
+				
+func receive_knockback(push_dir: float) -> void:
+	velocity.y = -150
+	
+	velocity.x = push_dir * 800
+	
 func _ensure_gameplay_actions() -> void:
 	_add_key_to_action(&"move_left", KEY_A)
 	_add_key_to_action(&"move_left", KEY_LEFT)
@@ -79,6 +76,10 @@ func _ensure_gameplay_actions() -> void:
 	_add_key_to_action(&"jump", KEY_SPACE)
 	_add_key_to_action(&"jump", KEY_W)
 	_add_key_to_action(&"jump", KEY_UP)
+	
+	# essas aqui são pra empurrar o coleguinha
+	_add_key_to_action(&"push", KEY_E)
+	_add_key_to_action(&"push", KEY_SHIFT)
 
 
 func _add_key_to_action(action: StringName, keycode: Key) -> void:
